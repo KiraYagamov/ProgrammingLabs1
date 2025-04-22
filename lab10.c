a71c7328e6cd062baf08fce7b44d96a6b837a773
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2025-04-22 21:41:46 +0300

    upd

--- lab10.c
+++ lab10.c
@@ -85,7 +85,25 @@ int main(){
 //    lab10task3();
 //    lab10task4();
 //    lab10task5();
-    lab10task6();
+//    lab10task6();
 //    lab10task7();
+
+    int dim = 4;
+    int i,j,N[dim],n[dim],num;
+    num=1;
+    for(i=0; i<dim; i++) {
+        N[i] = i+2; num *= N[i];n[i]=0;
+    }
+    for(i=0;i<num;i++) {
+        for(j=0;j<dim;j++) {
+            printf("%d ",n[j]);
+        }
+        printf("\n");
+        j=0;
+        do {
+            n[j] = (n[j]+1) % N[j];
+        } while(!n[j] && ++j<dim);
+    }
+
     return 0;
 }
